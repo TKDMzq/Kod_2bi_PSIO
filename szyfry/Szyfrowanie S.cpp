675ba6dbfc53a8d354be3d47b3632c0daf822f13
Adrian Mokrzycki <43567676+SzeryfKrul@users.noreply.github.com>
2018-12-10 14:48:19 +0100

    Szyfrowanie S

--- szyfry/Szyfrowanie S.cpp
+++ szyfry/Szyfrowanie S.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
   string s, w;
-  cout<<"Podaj slowo(w): "<<endl;
+  cout << "Podaj slowo(w): " << endl;
   int i;
   char k;
   getline(cin,s);
@@ -14,7 +14,7 @@ int main()
   for(i = 0; i < s.length(); i++)
     if(i % 2)
     {
-       swap(w[i],w[i-1]);
+       swap(w[i],w[i - 1]);
     }
   cout << w << endl;
   return 0;
