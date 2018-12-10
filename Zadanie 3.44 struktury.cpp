ace1013f3aa5a345c249bcf064c74112cbfbc898
Adrian Mokrzycki <43567676+SzeryfKrul@users.noreply.github.com>
2018-12-10 14:41:57 +0100

    Zadanie struktury na ocenke

--- Zadanie 3.44 struktury.cpp
+++ Zadanie 3.44 struktury.cpp
@@ -2,51 +2,91 @@
 
 using namespace std;
 
-struct przelot 
+struct przelot
 {
     string trasa, linia;
-    int cena, ilosc;
-    float czas;
+    float cena, czas, ilosc;
 };
 
 int main()
 {
-    int i, j, k, o, dod;
-    przelot tab [5];
-    for(i = 0; i < 5; i++)
+    int i, suma = 0, n, srednia, p;
+    float mx, b;
+    const int m = 5;
+    cout << "Podaj ilosc przelotow: " << endl;
+    cin >> n;
+    while (n > m)
     {
-        cout << "Podaj trase: " << endl;
+        cout << "Za duza ilosc przelotow!" << endl;
+        cout << "Podaj ilosc przelotow: " << endl;
+        cin >> n;
+        if (n < m)
+        {
+            break;
+        }
+    }
+    przelot tab[n];
+    cout << "Podpunkt A) --------------------" << endl;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Trasa nr." << i + 1 << endl;
+        cout << "Podaj cel lotu: " << endl;
         cin >> tab[i].trasa;
-        cout << "Podaj cene przelotu: " << endl;
+        cout << "Podaj cene biletu: " << endl;
         cin >> tab[i].cena;
-        cout << "Podaj dlugosc lotu (w kilometrach): " << endl;
+        cout << "Podaj dlugosc trasy: (km)" << endl;
         cin >> tab[i].ilosc;
-        cout << "Podaj czas lotu (w godzinach): " << endl;
+        cout << "Podaj czas lotu: (godziny)" << endl;
         cin >> tab[i].czas;
-        cout << "Podaj nazwe lini lotniczej): " << endl;
+        cout << "Podaj nazwe linii: " << endl;
         cin >> tab[i].linia;
-        cout << "____________________" << endl;
     }
-    for (j = 0; j < sizeof(tab); j++)
+    cout << "Podpunkt 😎 --------------------" << endl;
+    for (i = 0; i < n; i++)
     {
-        cout << "trasa to: " << tab[j].trasa << endl;
-        cout << "cena to: " << tab[j].cena << endl;
-        cout << "dlugosc lotu w kilometrach to: " << tab[j].ilosc << endl;
-        cout << "czas przelotu to: " << tab[j].czas << endl;
-        cout << "linia to: " << tab[j].linia << endl;
-        cout << "__________________" << endl;
+        cout << "Cel lotu: " << tab[i].trasa << endl;
+
+        cout << "Cena biletu: " << tab[i].cena << endl;
+
+        cout << "Dlugosc trasy: " << tab[i].ilosc << endl;
+
+        cout << "Czas lotu: " << tab[i].czas << endl;
+
+        cout << "Nazwa lini: " << tab[i].linia << endl;
+
+        cout << endl;
     }
-    for (k = 0; k < sizeof(tab); k++)
+    cout << "Podpunkt C) --------------------" << endl;
+    for (i = 0; i < n; i++)
     {
-        cout << tab[k].trasa << endl;
-        cout << tab[k].cena << endl;
-        cout << "_________________" << endl;
+        cout << "Cel lotu: " << tab[i].trasa << endl;
+
+        cout << "Cena biletu: " << tab[i].cena << endl;
     }
-    for (o = 0; o < sizeof(tab); o++)
+    cout << "Podpunkt D) --------------------" << endl;
+    for (i = 0; i < n; i++)
     {
-        dod = tab[l].cena + tab[o + 1].cena;
+        suma = tab[i].cena + suma;
     }
-    int sr = dod/sizeof(tab);  //liczenie sredniej ceny bietow
-    cout << "srednia cena biletow na wszystkich liniach to: " << sr << endl;
+    srednia = suma / n;
+    cout << "Srednia cen biletow wynosi: " << srednia << endl;
+    cout << "Podpunkt E) --------------------" << endl;
+    mx = tab[0].ilosc;
+    for (i = 1; i < n; i++)
+    {
+        if (mx < tab[i].ilosc)
+        {
+            mx = tab[i].ilosc;
+            p = i;
+        }
+    }
+    cout << "Najdluzsza trasa: " << tab[p].trasa << endl;
+    cout << "Dlugosc: " << mx << endl;
+    cout << "Podpunkt F) --------------------" << endl;
+    for (i = 0; i < n; i++)
+    {
+        b = tab[i].cena / tab[i].ilosc;
+        cout << "Cena za 1 km trasy " << i + 1 << " wynosi: " << b << endl;
+    }
     return 0;
 }
