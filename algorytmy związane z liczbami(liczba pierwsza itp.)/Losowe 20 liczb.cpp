fff5674059ea0aa241e4cd6652150681cbb3f6be
Adrian Mokrzycki <43567676+SzeryfKrul@users.noreply.github.com>
2018-12-10 15:03:15 +0100

    Losowe 20 liczb

--- algorytmy związane z liczbami(liczba pierwsza itp.)/Losowe 20 liczb.cpp
+++ algorytmy związane z liczbami(liczba pierwsza itp.)/Losowe 20 liczb.cpp
@@ -3,20 +3,16 @@
 #include <cstdlib>
 
 using namespace std;
+
 int main()
 {
     int tab[20], i;
 	srand(time(NULL));
-    cout<<"Wylosowane liczby to: "<<endl;
-	for (i = 0; i < 20; ++i)
+    cout << "Wylosowane liczby to: " << endl;
+	for (i = 0; i < 20; i++)
 	{
-
-
-			tab[i] = rand() % 100;
-			cout<<tab[i]<<" ";
-
-	}
-
+        tab[i] = rand() % 100;
+        cout << tab[i] << " ";
+    }
 	return 0;
-
 }
