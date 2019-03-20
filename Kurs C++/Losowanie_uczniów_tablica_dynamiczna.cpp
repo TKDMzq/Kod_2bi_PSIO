7129c6c980c49f655b87993feaa1bf41ababac6a
Adrian Mokrzycki <amokrzycki96@gmail.com>
2019-03-20 18:42:01 +0100

    Test

--- Kurs C++/Losowanie_uczniów_tablica_dynamiczna.cpp
+++ Kurs C++/Losowanie_uczniów_tablica_dynamiczna.cpp
@@ -9,24 +9,24 @@ int main ()
 {
 	fstream plik;
 	string trash;
-	int i = 0, j = 0;
+	int liczniklin = 0, j = 0;
 	srand(time(NULL));
 	plik.open("Uczniowie.txt", ios::in);
 	while (plik >> trash)
 	{
-		i++;
+		liczniklin++;
 	}
 	plik.close();
-	i = i / 2;
-	cout << i << endl;
+	liczniklin = liczniklin / 2;
+	cout << liczniklin << endl;
 	plik.open("Uczniowie.txt", ios::in);
-	string * klasa = new string[i];
-	for (j = 0; j < i; j++)
+	string * klasa = new string[liczniklin];
+	for (j = 0; j < liczniklin; j++)
 	{
 		getline (plik, klasa[j]);
 		cout << klasa[j] << endl;
 	}
-	cout << "Wylosowany uczen to: " << klasa[rand () % i];
+	cout << "Wylosowany uczen to: " << klasa[rand () % liczniklin];
 	delete [] klasa;
 	plik.close();
 	return 0;
