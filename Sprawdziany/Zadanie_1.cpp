e4f4629ab0f2edce03ee0ae985c7f7d9c2c5766d
Adrian Mokrzycki <43567676+SzeryfKrul@users.noreply.github.com>
2019-03-24 19:11:49 +0100

    Create Zadanie_1.cpp

---
+++ Sprawdziany/Zadanie_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct faktura
+{
+    float cena;
+    int year, id;
+};
+
+int main()
+{
+    int i;
+    faktura tab[5];
+    for(i = 0; i < 5; i++)
+    {
+        cout << "Podaj rok: ";
+        cin >> tab[i].year;
+        cout << "Podaj cene: ";
+        cin >> tab[i].cena;
+        cout << "Podaj indentyfikator klienta: ";
+        cin >> tab[i].id;
+    }
+    for(i = 0; i < 5; i++)
+    {
+        if (tab[i].year >= 2019 && tab[i].cena > 200)
+        {
+            cout << tab[i].cena << " ";
+            cout << tab[i].id;
+        }
+    }
+    return 0;
+}
